723e948fe2ecde97cfe8f8ab4bee3768c313ce2f
samuelitwaru <samuelitwaru@gmail.com>
2024-05-30 08:28:43 +0300

    5-30-24

--- munirif/settings.py
+++ munirif/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'background_task',
+    # 'background_task',
     'accounts',
     'core',
     'corsheaders',
